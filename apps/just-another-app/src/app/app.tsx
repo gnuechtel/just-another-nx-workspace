19ae784c8f1ce587a4f2f47537700a156eda5022
Christian Gnüchtel <christian.gnuechtel@adesso.de>
2022-05-30 02:00:20 +0200

    initial commit

---
+++ apps/just-another-app/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="just-another-app" />
+      <div />
+    </>
+  );
+}
+
+export default App;
